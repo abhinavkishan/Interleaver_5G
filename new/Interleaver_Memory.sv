c190dc455aa717aa0fbb767573d212036506a042
Thrinath Sai Unnam <143892069+thrinath12@users.noreply.github.com>
2026-05-09 02:24:33 -0400

    Update Interleaver_Memory.sv

--- new/Interleaver_Memory.sv
+++ new/Interleaver_Memory.sv
@@ -43,71 +43,55 @@ module Interleaver_Memory(
     end
 
 
-    logic [47:0] S1_DATA_IN;
-    logic [47:0] S1_DATA_OUT;
-    logic        S1_DATA_WE;
-    logic [1:0] S1_DATA_ADDRESS;
-
-    logic [47:0] S2_DATA_IN;
-    logic [47:0] S2_DATA_OUT;
-    logic        S2_DATA_WE;
-    logic [1:0] S2_DATA_ADDRESS;
 
 
-    logic [9:0] A1_DATA_IN;
-    logic [9:0] A1_DATA_OUT;
-    logic        A1_DATA_WE;
-    logic [1:0] A1_DATA_ADDRESS;
+    logic [47:0] storage_in[0:1];
+    logic [47:0] storage_out[0:1];
+    logic        storage_we[0:1];
+    logic [1:0] storage_address[0:1];
 
-    logic [9:0] A2_DATA_IN;
-    logic [9:0] A2_DATA_OUT;
-    logic        A2_DATA_WE;
-    logic [1:0] A2_DATA_ADDRESS;
+    logic [5:0] count_in[0:1];
+    logic [5:0] count_out[0:1];
+    logic        count_we[0:1];
+    logic [1:0] count_address[0:1];
+
+    logic [11:0] addr_in[0:1];
+    logic [11:0] addr_out[0:1];
+    logic        addr_we[0:1];
+    logic [1:0] addr_address[0:1];
 
 
         
-    LUT_RAM S1 (
-        .clk(ap_clk),
-        .we(S1_DATA_WE),    // Write Enable
-        .addr(S1_DATA_ADDRESS),  // Address bus
-        .din(S1_DATA_IN),   // Data input
-        .dout(S1_DATA_OUT)   // Data output
-    );
+    LUT_RAM S1 (.clk(ap_clk),.we(storage_we[0]),.addr(storage_address[0]),.din(storage_in[0]),.dout(storage_out[0]));
+    LUT_RAM S2 (.clk(ap_clk),.we(storage_we[1]),.addr(storage_address[1]),.din(storage_in[1]),.dout(storage_out[1]));
 
+    LUT_RAM #(.DATA_WIDTH(6)) C1 (.clk(ap_clk),.we(count_we[0]),.addr(count_address[0]),.din(count_in[0]),.dout(count_out[0]));
+    LUT_RAM #(.DATA_WIDTH(6)) C2 (.clk(ap_clk),.we(count_we[1]),.addr(count_address[1]),.din(count_in[1]),.dout(count_out[1]));
 
-    LUT_RAM S2 (
-        .clk(ap_clk),
-        .we(S2_DATA_WE),    // Write Enable
-        .addr(S2_DATA_ADDRESS),  // Address bus
-        .din(S2_DATA_IN),   // Data input
-        .dout(S2_DATA_OUT)   // Data output
-    );
+    LUT_RAM #(.DATA_WIDTH(12)) A1(.clk(ap_clk),.we(addr_we[0]),.addr(addr_address[0]),.din(addr_in[0]),.dout(addr_out[0]));
+    LUT_RAM #(.DATA_WIDTH(12)) A2(.clk(ap_clk),.we(addr_we[1]),.addr(addr_address[1]),.din(addr_in[1]),.dout(addr_out[1]));
+   ///////////////////////////////  CORE LOGIC /////////////////////////////////////////////////////
 
+    logic pointer_load;
 
-    LUT_RAM #(.DATA_WIDTH(10)) A1(
-        .clk(ap_clk),
-        .we(A1_DATA_WE),    // Write Enable
-        .addr(A1_DATA_ADDRESS),  // Address bus
-        .din(A1_DATA_IN),   // Data input
-        .dout(A1_DATA_OUT)   // Data output
-    );
-    LUT_RAM #(.DATA_WIDTH(10)) A2(
-        .clk(ap_clk),
-        .we(A2_DATA_WE),    // Write Enable
-        .addr(A2_DATA_ADDRESS),  // Address bus
-        .din(A2_DATA_IN),   // Data input
-        .dout(A2_DATA_OUT)   // Data output
-    );
-
-    /////////////////////////////// EXTERNAL MEMORY HANDLING //////////////////////////
-
-    
-
-
-
-   ///////////////////////////////  CORE LOGIC //////////////////////////////////////
-
-    logic [47:0] DATA[0:1][0:7];
+    always_comb begin
+        
+    end
+    always_ff@(posedge ap_clk)begin
+        if(pointer_load)begin
+            case (Qm)
+                2:begin
+                    
+                end 
+                4: 
+                6: 
+                default: 
+            endcase
+        end
+        else begin
+            
+        end
+    end
     
 
 
@@ -316,167 +300,3 @@ module LUT_RAM #(
 
 endmodule
 
-
-/*
-
-
-logic [31:0] mem_reg[0:7];
-logic mem_update;
-logic mem_select;
-
-always_ff@(posedge ap_clk)begin
-
-    if(!ap_rst_n) begin
-        mem_update <=0;
-        mem_select <=0;
-    end
-    if(mem_update)begin
-        for(int i=0;i<8;i++)begin
-            mem_reg[i] <= {Din[i][2*mem_select],Din[i][2*mem_select+1]};
-        end
-    end
-end
-
-
-logic [1:0] state;
-
-logic [9:0] address_array[0:7];
-logic [3:0] storage_count_array[0:7];
-logic [14:0] storage_array[0:7];
-logic [1:0] cs_array[0:7];
-
-
-
-wire [1:0]s1 = 0;
-wire [1:0]s2 = 1;
-wire [1:0]s3 = 2;
-wire [1:0]s4 = 3;
-
-logic [2:0] p;
-
-always_ff@(posedge ap_clk)begin
-    if(!ap_rst_n)begin
-        p <= 0;
-    end
-    else if(state==s1 && storage_valid)begin
-        storage_array[p] <= storage_in;
-        storage_count_array[p] <= storage_in_keep;
-        address_array[p] <= starting_address[11:2];
-        cs_array[p] <= starting_address[1:0];
-        p <= p + 1;
-    end
-
-end
-
-///////////////////////////////////////////////// UNIT 0 /////////////////////////////////////////////////
-/*
-logic Address[0:3][0:1];
-
-
-logic [63:0] Backup_unit0[0:1];
-logic Backup_valid;
-
-logic row_address[0:3];
-logic row_data[0:3];
-
-logic [1:0] cs_address[0:3];
-logic [1:0] cs_data[0:3];
-
-logic [3:0] storage_count[0:3];
-logic [14:0] storage[0:3];
-
-always_comb begin
-
-    for(int i=0;i<4;i++)begin
-        if(row_address[i]==0) begin
-            Address[i][0] = address_array[2*i];
-            cs_address[i] = cs_array[2*i];
-        end
-        else begin
-            Address[i][0] = address_array[2*i+1];
-            cs_address[i] = cs_array[2*i+1];
-        end
-
-        Address[i][1] = Address[i][0] +1;
-    end
-
-    for(int i=0;i<4;i++)begin
-
-        if(cs_address[i]>=1) RAM_Address[i][0] = Address[i][1];
-        else RAM_Address[i][0] = Address[i][0];
-
-        if(cs_address[i]>=2) RAM_Address[i][0] = Address[i][1];
-        else RAM_Address[i][0] = Address[i][0];
-
-        if(cs_address[i]==3) RAM_Address[i][0] = Address[i][1];
-        else RAM_Address[i][0] = Address[i][0];
-
-        RAM_Address[i][3] = Address[i][0];
-    end
-end
-
-
-always_ff@(posedge ap_clk)begin
-    for(int i=0;i<4;i++)begin
-        cs_data[i] <= cs_address[i];
-        row_data[i] <= row_address[i];
-    end
-end
-
-
-always_comb begin
-
-    for(int i=0;i<4;i++)begin
-        if(row_data[i]==0)begin
-            storage[i]= storage_array[2*i];
-            storage_count[i]= storage_count_array[2*i];
-        end
-        else begin
-            storage[i] = storage_array[2*i+1];
-            storage_count[i] = storage_count_array[2*i+1];
-        end
-    end
-end
-
-always_ff@(posedge ap_clk)begin
-    if(state==s1 && storage_valid)begin
-        address_array[p] <= starting_address;
-    end
-    else begin
-        
-    end
-    
-end
-*/
-//////////////////// OUTPUT //////////////////////////////////////////
-
-/*
-
-    logic [15:0] out_wire[0:7];
-    logic [14:0] storage[0:7];
-    logic [3:0] storage_bit_count[0:7];
-    logic [15:0] data[0:7];
-    logic [2:0] Qm;
-    always_comb begin
-        if(Qm==8)begin
-            for(int i=0;i<8;i++)begin
-                out_wire[i] ={storage[i],1'b0} | (data[i]>>storage_bit_count);
-            end
-        end    
-    
-    end
-
-
-
-    logic output_valid;
-    logic output_update;
-
-    always_ff@(posedge ap_clk)begin
-        if(!ap_rst_n) output_valid <=0;
-        else if(output_update) begin
-            
-        end
-    end
-
-
-    */
