d6986b2bc00519f1cc98504bb8637e6678f97f93
Thrinath Sai Unnam <143892069+thrinath12@users.noreply.github.com>
2026-05-09 10:01:24 +0530

    Update Packer.sv

--- new/Packer.sv
+++ new/Packer.sv
@@ -26,6 +26,7 @@ module Packer(
     logic in_valid_reg;
     logic [127:0] in_data_reg;
     logic [6:0] in_keep_reg;
+    logic in_last_reg;
 
     logic read_over;
     assign inData_tready = ap_rst_n && (!read_over);
@@ -42,6 +43,7 @@ module Packer(
         end
         else begin
             in_valid_reg <=0;
+            in_last_reg <= 0;
         end
     end
 ///////////////////////////////////// INPUT END LOGIC ///////////////////////////////////////
@@ -67,25 +69,32 @@ module Packer(
     end
     
       
+/////////////////////////////////////////////
 
+    
 
     logic [127:0] buffer;    
     logic [6:0]   free;     
     logic spill;
-
+    logic write_final;
+    always_ff@(posedge ap_clk) write_final <= in_last_reg;
+    
     always_comb begin
-        spill   = 1'b0;
-        if(read_over && !write_over) spill = 1;
-        if (free<in_keep_reg)  spill   = 1;
+        spill   = 0;
+        if(write_final) spill =1;
+        else if(in_valid_reg && free<=in_keep_reg) spill   = 1;
     end
     ////////////////////////////////////// SHIFTING LOGIC ////////////////////////////////////
     
+    wire [7:0] lsc_temp = 128-in_keep_reg+free;
 
-    logic [6:0] left_shift_count = spill?(free+1):(free-in_keep_reg);
+    wire[6:0] left_shift_count = spill?lsc_temp:(free-in_keep_reg);
     wire [127:0] left_shift_out = in_data_reg << left_shift_count;;
 
     wire [6:0] right_shift_count = in_keep_reg-free;
     wire [127:0] right_shift_out = in_data_reg >> right_shift_count;
+
+    /// 127 - (k-1) .  k = free - tkeep;  127 - (free - tkeep-1) = 
 
 
     
@@ -95,7 +104,7 @@ module Packer(
     logic write_over;
     always_ff@(posedge ap_clk) begin
         if(!ap_rst_n) write_over <= 0;
-        else write_over <= read_over;
+        else write_over <= in_last_reg;
     end
    
     always_ff @(posedge ap_clk ) begin
@@ -103,10 +112,10 @@ module Packer(
             buffer <= '0;
             free <= 127;
         end 
-        else if (in_valid_reg && (!write_over)) begin
-            if (spill)  begin
-                outData_tdata <= buffer | right_shift_out;
-                buffer <= left_shift_out;
+        else if (in_valid_reg) begin
+            if (spill)  begin   
+                if(free == in_keep_reg) buffer <= 0;   
+                else buffer <= left_shift_out;
                 free <=127 + free-in_keep_reg;
             end    
             else begin
@@ -114,23 +123,26 @@ module Packer(
                 free <= free -in_keep_reg -1;
             end    
         end
+    end
+
+
+    always_ff @(posedge ap_clk ) begin
         
+        if(write_final) outData_tdata <= buffer;
+        else if (spill)  outData_tdata <= buffer | right_shift_out;   
 
-
+        
         if(!ap_rst_n) outData_tvalid <= 0;
-        else if(in_valid_reg && (!write_over) && spill)outData_tvalid <= 0;
+        else if(spill)outData_tvalid <= 1;
         else outData_tvalid <= 0;
     end
+
     
     
     always@(posedge ap_clk) begin
-    
         if(!ap_rst_n) N<=0;
-        else if(spill) begin
-            if(read_over) N <= N + 7'd127-free;
-            else N <= N+128;
-        end
-    
+        else if(write_final) N <= N + 7'd127-free;
+        else if(spill)N <= N+128;
     end
 
 
@@ -141,6 +153,3 @@ module Packer(
     
 
 endmodule
-
-
-
